20821a055105dd77f62eabe8a79522d18215fc19
Money-2021 <10fe82.80348a@gmail.com>
2021-09-12 15:01:09 +1000

    Aligned to ECIES dhSinglePass-stdDH-sha256kdf for public domain code base.

--- Payment Client/EphemeralStaticDiffieHellman.cs
+++ Payment Client/EphemeralStaticDiffieHellman.cs
@@ -13,30 +13,28 @@ namespace Payment_Client
     public class EphemeralStaticDiffieHellman
     {
        
+
         public static byte[] EncryptData( byte[] staticPublicKey, byte[] data, out byte[] ephemeralPUblicKey )
         {
             using (ECDiffieHellmanCng ephemeraKey = new ECDiffieHellmanCng())
             {
 
-                ephemeraKey.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                ephemeraKey.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash; // RFC 5753: dhSinglePass-stdDH-sha256kdf 
                 ephemeraKey.HashAlgorithm = CngAlgorithm.Sha256;
                 // Save EphemeralPUblicKey
                 ephemeralPUblicKey = ephemeraKey.PublicKey.ToByteArray();
                 CngKey recipientKey = CngKey.Import(staticPublicKey, CngKeyBlobFormat.EccPublicBlob);
-                byte[] keyAndIv = ephemeraKey.DeriveKeyMaterial(recipientKey);
-                // Derive AES Key and IV
-                byte[] key = new byte[16];
-                Array.Copy(keyAndIv, 0, key, 0, 16);
-                byte[] iv = new byte[16];
-                Array.Copy(keyAndIv, 16, iv, 0, 16);
-                // Preform Ecnryption
-                Aes aes = new AesManaged();
-                aes.Key = key;
-                aes.IV = iv;
-                aes.Mode = CipherMode.CBC;
-                aes.Padding = PaddingMode.PKCS7; // Old world default
-                return  aes.CreateEncryptor().TransformFinalBlock(data, 0, data.Length);
-
+                byte[] derivedKey = ephemeraKey.DeriveKeyMaterial(recipientKey);
+                // ECIES uses AES with the all zero IV.
+                using (Aes aes = new AesManaged())
+                {
+                    aes.Key = derivedKey;
+                    aes.IV = null;
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7; // Old world default
+                    return aes.CreateEncryptor().TransformFinalBlock(data, 0, data.Length);
+                }
+              
             }  
         }
         
